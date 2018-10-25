892bc9b359ef4424b90d99208269fb66313ab909
IFMobile <ifmobilefoz@gmail.com>
2018-10-25 16:30:16 -0300

    Modificações para versão 2

--- app/src/main/java/com/example/professor/fragmentsdinamicos/MainActivity.java
+++ app/src/main/java/com/example/professor/fragmentsdinamicos/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.professor.fragmentsdinamicos;
 
+import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,17 +13,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        LeftFragment leftFragment = new LeftFragment();
-        RightFragment rightFragment = new RightFragment();
-
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.fragment1, leftFragment,"Left");
-        fragmentTransaction.add(R.id.fragment2, rightFragment,"Right");
-        fragmentTransaction.commit();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        if (item.getItemId() == R.id.add_frag1){
+            LeftFragment leftFragment = new LeftFragment();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.fragment1, leftFragment,"Left");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+            return true;
+        }
+        else if (item.getItemId() == R.id.add_frag2){
+            RightFragment rightFragment = new RightFragment();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.fragment2, rightFragment, "Right");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+            return true;
+        }
+        else if(item.getItemId() == R.id.del_frag1){
+            LeftFragment leftFragment = (LeftFragment)fragmentManager.findFragmentById(R.id.fragment1);
+            if (leftFragment != null){
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.remove(leftFragment);
+                fragmentTransaction.commit();
+            }
+            return true;
+        }
+        else if(item.getItemId() == R.id.del_frag2){
+            RightFragment rightFragment = (RightFragment)fragmentManager.findFragmentById(R.id.fragment2);
+            if (rightFragment != null){
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.remove(rightFragment);
+                fragmentTransaction.commit();
+            }
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
